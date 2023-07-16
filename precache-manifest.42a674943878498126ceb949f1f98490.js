73a88bea2c28ac439692c4f589d200a83aed2885
Tim Yeung <cmyeung@gmail.com>
2023-07-16 17:36:07 +0800

    Updates

---
+++ precache-manifest.42a674943878498126ceb949f1f98490.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "489ab7c17f83e272fb36",
+    "url": "/react_tictactoe/static/css/main.1d847b9e.chunk.css"
+  },
+  {
+    "revision": "489ab7c17f83e272fb36",
+    "url": "/react_tictactoe/static/js/main.489ab7c1.chunk.js"
+  },
+  {
+    "revision": "c4427d1154b5428f2e63",
+    "url": "/react_tictactoe/static/js/1.c4427d11.chunk.js"
+  },
+  {
+    "revision": "110e1c42898f58155167",
+    "url": "/react_tictactoe/static/js/runtime~main.110e1c42.js"
+  },
+  {
+    "revision": "13706fd35ee65a6d604c113a2c990960",
+    "url": "/react_tictactoe/index.html"
+  }
+];
